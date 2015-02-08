419ebac6e82bd900d17823338fb4efceda4d4535
techjanitor <puntme@gmail.com>
2015-02-07 22:56:57 -0600

    Initial

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuration items
+prim.constant('config', {
+  // Set imageboard id 
+  ib_id: 1,
+  // Set title for pages
+  title: 'Imageboard',
+  // Set antispam key
+  as_key: 'whatever',
+  // Set address
+  site_addr: 'http://imageboard.com'
+});

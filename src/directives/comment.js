b150fd0590630530742eec7e410c794be7696e14
techjanitor <puntme@gmail.com>
2015-07-26 01:54:44 -0500

    improve quote id handler

--- src/directives/comment.js
+++ src/directives/comment.js
@@ -1,5 +1,6 @@
 // comment-handler uses a regex to parse out quotes from a post starting with >> and adds them to an array
 // it then sends the generated quotes to the quotebox directive which will create a popup with the posts
+// this is based on the linky filter from ngSanitize
 angular.module('prim').directive('commentHandler', function() {
     return {
         restrict: 'A',
@@ -9,39 +10,43 @@ angular.module('prim').directive('commentHandler', function() {
         },
         templateUrl: "pages/comment.html",
         controller: function($scope) {
-            // Match quote format
+
+            // array for comment text
+            var comment = [];
+            // array for quote ids
+            var quotes = [];
+
+            // matches >> with up to 5 digits
             var re = /(>>(\d{1,5}))/;
+            // raw comment from json
             var raw = $scope.post.comment;
-            var html = [];
-            var id = [];
+            // match from regex on comment
             var match;
 
-            function addText(text) {
-                html.push(text);
-            }
-
-            // makes a link
-            function addLink(url) {
-                id.push(url);
-            }
-
             // Run regex on comment and grab matches
-            while (match = raw.match(re)) {
+            while ((match = raw.match(re)) !== null) {
                 // Get second group (just the post num)
-                var url = match[2];
+                var id = match[2];
+                // add quote id to id array
+                quotes.push(id);
+
+                // index of match
                 var i = match.index;
                 // Add non matches to html array
-                addText(raw.substr(0, i));
-                // Create link with matches
-                addLink(url, match[0]);
+                comment.push(raw.substr(0, i));
+
+                // this sets raw to the remainder text
                 raw = raw.substring(i + match[0].length);
             }
 
-            addText(raw);
-            // join the html array and sanitize
-            $scope.post.comment = html.join('');
-            // add post num to scope
-            $scope.post.quote_id = id;
+            // push for final loop
+            comment.push(raw);
+
+            // this joins all the comment text sans quote ids
+            $scope.post.comment = comment.join('');
+
+            // this is all the collected quote ids
+            $scope.post.quote_id = quotes;
 
         }
     };

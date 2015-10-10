fb15a0d06d7bd56ad30d18c982327c4c917eae34
techjanitor <puntme@gmail.com>
2015-10-10 17:57:47 -0500

    update index.html and angular version

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ angular.module('prim').constant('config', {
     // Set imageboard id 
     ib_id: 1,
     // Set title for pages
-    title: 'Whatever',
+    title: 'Prim',
     // image server
-    img_srv: '//images.whatever.fart',
+    img_srv: '//images.website.com',
     // api server
-    api_srv: '//api.whatever.fart'
+    api_srv: '//api.website.com'
 });

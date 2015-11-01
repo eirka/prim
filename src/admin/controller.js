e35accc1b1c0d8d735d8a4f63a6503bef5d4fd2d
techjanitor <puntme@gmail.com>
2015-10-31 23:02:15 -0500

    controller for mod functions on posts

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -1,5 +1,5 @@
 // mod functions for threads
-angular.module('prim').controller('ThreadModCtrl', function($scope, $location, $route, toaster, ThreadDeleteHandler, PostDeleteHandler, CloseThreadHandler, StickyThreadHandler, Utils) {
+angular.module('prim').controller('ThreadModCtrl', function($location, $route, toaster, ThreadDeleteHandler, CloseThreadHandler, StickyThreadHandler, Utils) {
 
     // using controllerAs
     var self = this;
@@ -28,20 +28,7 @@ angular.module('prim').controller('ThreadModCtrl', function($scope, $location, $
         });
     };
 
-    // delete a post
-    self.deletePost = function(thread_id, post_id) {
-        if (confirm("Are you sure you want to delete this post?")) {
-            PostDeleteHandler.remove({
-                thread: thread_id,
-                id: post_id
-            }, function(data) {
-                $route.reload();
-                toaster.pop('success', data.success_message);
-            }, function(error) {
-                toaster.pop('error', error.data.error_message);
-            });
-        };
-    };
+
 
     // delete a thread
     self.deleteThread = function(thread_id) {
@@ -50,6 +37,30 @@ angular.module('prim').controller('ThreadModCtrl', function($scope, $location, $
                 id: thread_id
             }, function(data) {
                 $location.path('/');
+                toaster.pop('success', data.success_message);
+            }, function(error) {
+                toaster.pop('error', error.data.error_message);
+            });
+        };
+    };
+
+});
+
+
+// mod functions for threads
+angular.module('prim').controller('PostModCtrl', function($route, toaster, PostDeleteHandler, Utils) {
+
+    // using controllerAs
+    var self = this;
+
+    // delete a post
+    self.deletePost = function(thread_id, post_id) {
+        if (confirm("Are you sure you want to delete this post?")) {
+            PostDeleteHandler.remove({
+                thread: thread_id,
+                id: post_id
+            }, function(data) {
+                $route.reload();
                 toaster.pop('success', data.success_message);
             }, function(error) {
                 toaster.pop('error', error.data.error_message);

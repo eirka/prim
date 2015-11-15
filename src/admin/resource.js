c65a57a52495042760b4acd2b76802c5d7c05b61
techjanitor <puntme@gmail.com>
2015-11-15 00:13:39 -0600

    bindonce in ngrepeat

--- src/admin/resource.js
+++ src/admin/resource.js
@@ -1,36 +1,36 @@
 angular.module('prim').service('ThreadDeleteHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/thread/:id', {
+    return $resource(config.api_srv + '/admin/mod/thread/:id', {
         id: '@id'
     });
 });
 
 angular.module('prim').service('PostDeleteHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/post/:thread/:id', {
+    return $resource(config.api_srv + '/admin/mod/post/:thread/:id', {
         thread: '@thread',
         id: '@id'
     });
 });
 
 angular.module('prim').service('CloseThreadHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/close/:id', {
+    return $resource(config.api_srv + '/admin/mod/close/:id', {
         id: '@id'
     });
 });
 
 angular.module('prim').service('StickyThreadHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/sticky/:id', {
+    return $resource(config.api_srv + '/admin/mod/sticky/:id', {
         id: '@id'
     });
 });
 
 angular.module('prim').service('TagDeleteHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/tag/:id', {
+    return $resource(config.api_srv + '/admin/mod/tag/:id', {
         id: '@id'
     });
 });
 
 angular.module('prim').service('ImageTagDeleteHandler', function($resource, config) {
-    return $resource(config.api_srv + '/post/mod/imagetag/:image/:tag', {
+    return $resource(config.api_srv + '/admin/mod/imagetag/:image/:tag', {
         image: '@image',
         tag: '@tag'
     });

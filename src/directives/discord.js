602f6074af02686bdaf3446f3f7688ac8aa70abe
techjanitor <puntme@gmail.com>
2016-06-15 15:03:24 -0500

    add discord widget

---
+++ src/directives/discord.js
@@ -0,0 +1,21 @@
+angular.module('prim').directive('discordWidget', function($resource, config) {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: "pages/discord.html",
+        link: function(scope) {
+
+            // the url for the discord widget json
+            var discord_endpoint = $resource(config.discord_widget, {});
+
+            // query the endpoint and get the data
+            discord_endpoint.get({}, function(data) {
+                // the instant invite link
+                scope.invite_link = data.instant_invite;
+                // the number of current users
+                scope.user_count = data.members.length;
+            });
+
+        }
+    };
+});
